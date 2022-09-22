d8f1cdb9f9f200adac8278a5a5e074a53dada550
雷超然 <leichaoran@conew.com>
2022-09-22 19:23:56 +0800

    现在sdk连不上底盘能力组件时，会报错提示用户从Launcher进入app

--- app/src/main/java/com/ainirobot/robotos/MainActivity.java
+++ app/src/main/java/com/ainirobot/robotos/MainActivity.java
@@ -26,6 +26,7 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 
 import com.ainirobot.coreservice.client.RobotApi;
+import com.ainirobot.robotos.fragment.FailedFragment;
 import com.ainirobot.robotos.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     private FrameLayout mContent;
 
     private static MainActivity mInstance;
+    private int checkTimes;
 
     public static MainActivity getInstance(){
         return mInstance;
@@ -42,19 +44,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkTimes = 0;
         initViews();
         mInstance = this;
     }
 
     private void initViews() {
         mContent = findViewById(R.id.container_content);
-        Fragment fragment = MainFragment.newInstance();
-        switchFragment(fragment);
+        checkInit();
     }
 
     public void switchFragment(Fragment fragment){
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.container_content, fragment, fragment.getClass().getName())
                 .commit();
+    }
+
+    private void checkInit(){
+        checkTimes++;
+        if(checkTimes > 10){
+            Fragment fragment = FailedFragment.newInstance();
+            switchFragment(fragment);
+        }
+        else if(RobotApi.getInstance().isApiConnectedService()){
+            Fragment fragment = MainFragment.newInstance();
+            switchFragment(fragment);
+        }else
+        {
+            mContent.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    checkInit();
+                }
+            },300);
+        }
     }
 }
